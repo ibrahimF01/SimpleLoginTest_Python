0ebfcbf43982f972f58f97d1ecb8f81ae8139cb2
ibrahimF01 <matrix012>
2022-11-23 14:32:16 +0300

    login method edit2

--- LoginTestMethod.py
+++ LoginTestMethod.py
@@ -13,8 +13,8 @@ def loginTest(userName, password, posOrNeg):
     driver.find_element(By.ID, "mat-input-0").send_keys(userName)
     driver.find_element(By.ID, "mat-input-1").send_keys(password)
     driver.find_element(By.CSS_SELECTOR, "button[aria-label='LOGIN']").click()
-    if (userName == "richfield.edu" and password == "ibrahimF") or (
-            userName == "ibrahimF" and password == "Richfield2020!") and posOrNeg == "negative":
+    if (userName == "richfield.edu" and password == "ibrahimF" and posOrNeg == "negative") or (
+            userName == "ibrahimF" and password == "Richfield2020!" and posOrNeg == "negative"):
         invalidMessage = driver.find_element(By.CSS_SELECTOR, "dynamic-view[class='ng-star-inserted']>div").text
         return invalidMessage
     if (userName == "richfield.edu" and
@@ -22,11 +22,17 @@ def loginTest(userName, password, posOrNeg):
         dashboardVerify = driver.find_element(By.CSS_SELECTOR, "div[class='ng-star-inserted']>span").text
         return dashboardVerify
 
-# Negative login test:
+# Negative login test1:
 if "Invalid username or password" in loginTest("richfield.edu", "ibrahimF", "negative"):
-    print("Negatif login test başarılı")
+    print("Negatif login test1 başarılı")
 else:
-    print("Negatif test başarısız")
+    print("Negatif test1 başarısız")
+
+# Negative login test2:
+if "Invalid username or password" in loginTest("ibrahimF", "Richfield2020!", "negative"):
+    print("Negatif login test2 başarılı")
+else:
+    print("Negatif test2 başarısız")
 
 #Pozitif login test:
 if "Dashboard" in loginTest("richfield.edu", "Richfield2020!", "positive"):
