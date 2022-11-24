05283e4e4a69fe6bf9b7f28e60a1de0d26074700
ibrahimF01 <matrix012>
2022-11-24 03:29:33 +0300

    login method assert

---
+++ LoginTestMethodAssert.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+service = Service("drivers/chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()  # sayfa tam ekran
+driver.implicitly_wait(30)
+
+
+def loginTest(userName, password, posOrNeg):
+    driver.get("https://demo.mersys.io/")
+    driver.find_element(By.ID, "mat-input-0").send_keys(userName)
+    driver.find_element(By.ID, "mat-input-1").send_keys(password)
+    driver.find_element(By.CSS_SELECTOR, "button[aria-label='LOGIN']").click()
+    if (userName == "richfield.edu" and password == "ibrahimF" and posOrNeg == "negative") or (
+            userName == "ibrahimF" and password == "Richfield2020!" and posOrNeg == "negative"):
+        invalidMessage = driver.find_element(By.CSS_SELECTOR, "dynamic-view[class='ng-star-inserted']>div").text
+        return invalidMessage
+    if (userName == "richfield.edu" and
+        password == "Richfield2020!") and posOrNeg == "positive":
+        dashboardVerify = driver.find_element(By.CSS_SELECTOR, "div[class='ng-star-inserted']>span").text
+        return dashboardVerify
+
+
+# Negative login test1:
+message = loginTest("richfield.edu", "ibrahimF", "negative")
+assert "Invalid username or password" in message
+
+# Negative login test2:
+message = loginTest("ibrahimF", "Richfield2020!", "negative")
+assert "Invalid username or password" in message
+
+# Pozitif login test:
+message = loginTest("richfield.edu", "Richfield2020!", "positive")
+assert "Dashboard" in message
+
+driver.quit()
